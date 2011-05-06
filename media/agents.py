c8017342299bae649fb39338333d6ba668b2377b
jose <jose@vancouver.(none)>
2011-05-05 21:44:00 -0430

    subiendo todo de nuevo

--- media/agents.py
+++ media/agents.py
@@ -3,3 +3,13 @@ class Agent:
     orientation = 0.0    # a single floating point value
     velocity = [0,0,0]   # another 2 or 3D vector
     rotation = 0.0       # a single floating point value
+
+    def update (self, steering, time):
+        
+        # Update the position and orientation
+        self.position += self.velocity * time
+        self.orientation += self.rotation * time
+
+        # and the velocity and rotation
+        self.velocity += steering.linear * time
+        self.orientation += steering.angular * time
