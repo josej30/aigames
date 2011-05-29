14dc5656308b8d0b6aaffa9ed5f4e17cdb292244
LilianaB <liliana.usb@gmail.com>
2011-05-29 17:48:33 -0430

    centro

--- structures/triangle.py
+++ structures/triangle.py
@@ -10,17 +10,18 @@ class Triangle:
     	def centerOfMass(self):
 		G = [0,0]
     		G[0] = (self.vertex1[0] + self.vertex2[0] + self.vertex3[0])/3.0
-    		G[1] = (self.vertex1[1] + self.vertex3[1] + self.vertex3[1])/3.0
+    		G[1] = (self.vertex1[1] + self.vertex2[1] + self.vertex3[1])/3.0
     		return G
 
-#p1 = Point()
+#p1 = [2,0]
 #p1.x = 2
 #p1.y = 0
-#p2 = Point()
+#p2 = [0,1]
 #p2.x = 0
 #p2.y = 1
-#p3 = Point()
+#p3 = [-3,-2]
 #p3.x = -3
 #p3.y = -2
 #t = Triangle(p1,p2,p3)
-#t.centerOfMass()
+#print t.centerOfMass()
+
